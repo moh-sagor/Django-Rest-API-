58b975f78bea43778198e41b0b6fd900e89875f8
Sagor Hossain <sagorhassain4@gmail.com>
2022-09-07 02:38:41 +0600

    rest api

---
+++ My_Api/status/views.py
@@ -0,0 +1,26 @@
+from .models import Status
+from .serializers import StatusSerializer
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from rest_framework import generics
+
+# Create your views here.
+
+
+class StatusAPIView(APIView):
+    def get(self, request, format=None):
+        status_list = Status.objects.all()
+        status_serializer = StatusSerializer(status_list, many=True)
+        return Response(status_serializer.data)
+
+
+class StatusListAPIView(generics.ListAPIView):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+
+
+class StatusCreateAPIView(generics.CreateAPIView):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
