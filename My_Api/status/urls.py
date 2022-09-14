2a718794270744dd1a8414669a7dcea4d83d7469
Sagor Hossain <sagorhassain4@gmail.com>
2022-09-15 02:37:06 +0600

    router viewset

--- My_Api/status/urls.py
+++ My_Api/status/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 from .views import (
-    StatusDetailUpdateDeleteAPIView,
-    StatusListCreateView,
+    # StatusDetailUpdateDeleteAPIView,
+    # StatusListCreateView,
+    StatusViewset,
 )
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r'status', StatusViewset, basename="status")
 
 urlpatterns = [
     # path('status/', StatusAPIView.as_view()),
-    path('status/', StatusListCreateView.as_view()),
-    path('status/<id>', StatusDetailUpdateDeleteAPIView.as_view()),
+    # path('status/', StatusListCreateView.as_view()),
+    # path('status/<id>', StatusDetailUpdateDeleteAPIView.as_view()),
 
-
-]
+]+router.urls
--- My_Api/status/views.py
+++ My_Api/status/views.py
@@ -1,30 +1,37 @@
 from .models import Status
 from .serializers import StatusSerializer
 
-from rest_framework import generics, parsers
+from rest_framework import (  # generics,
+    parsers, viewsets)
 
 # Create your views here.
 
 
-class StatusListCreateView(  # mixins.CreateModelMixin,
-        generics.ListCreateAPIView):
+# class StatusListCreateView(  # mixins.CreateModelMixin,
+#         generics.ListCreateAPIView):
+#     queryset = Status.objects.all()
+#     serializer_class = StatusSerializer
+#     parser_classes = [parsers.FormParser, parsers.MultiPartParser]
+
+#     # def post(self, request, *args, **kwargs):
+#     #     return self.create(request, *args, **kwargs)
+
+
+# class StatusDetailUpdateDeleteAPIView(  # mixins.UpdateModelMixin, mixins.DestroyModelMixin,
+#         generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Status.objects.all()
+#     serializer_class = StatusSerializer
+#     lookup_field = "id"
+#     parser_classes = [parsers.FormParser, parsers.MultiPartParser]
+
+#     # def put(self, request, *args, **kwargs):
+#     #     return self.update(request, *args, **kwargs)
+
+#     # def delete(self, request, *args, **kwargs):
+#     #     return self.destroy(request, *args, **kwargs)
+
+
+class StatusViewset(viewsets.ModelViewSet):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
     parser_classes = [parsers.FormParser, parsers.MultiPartParser]
-
-    # def post(self, request, *args, **kwargs):
-    #     return self.create(request, *args, **kwargs)
-
-
-class StatusDetailUpdateDeleteAPIView(  # mixins.UpdateModelMixin, mixins.DestroyModelMixin,
-        generics.RetrieveUpdateDestroyAPIView):
-    queryset = Status.objects.all()
-    serializer_class = StatusSerializer
-    lookup_field = "id"
-    parser_classes = [parsers.FormParser, parsers.MultiPartParser]
-
-    # def put(self, request, *args, **kwargs):
-    #     return self.update(request, *args, **kwargs)
-
-    # def delete(self, request, *args, **kwargs):
-    #     return self.destroy(request, *args, **kwargs)
