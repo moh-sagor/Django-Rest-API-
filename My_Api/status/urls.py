badc442f8b975c01b06f5551a7598b361a1111a3
Sagor Hossain <sagorhassain4@gmail.com>
2022-09-15 01:34:41 +0600

    mixins

--- My_Api/status/urls.py
+++ My_Api/status/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import StatusAPIView, StatusListAPIView, StatusCreateAPIView, StatusDetailAPIView, StatusUpdateAPIView, StatusDeleteAPIView
+from .views import (
+    StatusDetailAPIView,
+    StatusListCreateView,
+)
+
+
 urlpatterns = [
-    path('status/', StatusAPIView.as_view()),
-    path('status/list/', StatusListAPIView.as_view()),
-    path('status/create/', StatusCreateAPIView.as_view()),
-    path('status/detail/<id>/', StatusDetailAPIView.as_view()),
-    path('status/update/<id>/', StatusUpdateAPIView.as_view()),
-    path('status/delete/<id>/', StatusDeleteAPIView.as_view()),
+    # path('status/', StatusAPIView.as_view()),
+    path('status/', StatusListCreateView.as_view()),
+    path('status/<id>', StatusDetailAPIView.as_view()),
+
 
 ]
---
+++ script.js
@@ -0,0 +1,45 @@
+// C => Create => POST
+// R => Retrieve => GET
+// U => Update => PUT/PATCH
+// D => Delete => DELETE
+
+// GET Request
+// axios.get("http://127.0.0.1:8000/apiv1/status/list/")
+//     .then(response => console.log(response))
+
+// axios.get("http://127.0.0.1:8000/apiv1/status/detail/1/")
+//     .then(response => console.log(response))
+
+
+// POST Request
+// let status = {
+//     user: 1,
+//     content: "I am test data!",
+//     image: null
+// }
+// axios.post("http://127.0.0.1:8000/apiv1/status/create/", status, {
+//     headers: {
+//         "Content-Type": "application/json"
+//     }
+// })
+//     .then(response => console.log(response))
+//     .catch(error => console.log(error.message))
+
+
+// DELETE Request
+// axios.delete("http://127.0.0.1:8000/apiv1/status/delete/5/")
+//     .then(response => console.log(response))
+
+// PUT Request -> Whole Entity
+// PATCH Request -> Just the particular Property
+let u_status = {
+    user: 2,
+    content: "I have be updated using PATCH!",
+    image: null
+}
+axios.patch("http://127.0.0.1:8000/apiv1/status/update/7/", u_status, {
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+    .then(response => console.log(response))
